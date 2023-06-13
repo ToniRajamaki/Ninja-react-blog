edd312ce6c283fb03bbd8b829fc5f9379af65cb9
TONI <zingoo16@gmail.com>
2023-06-13 13:53:54 +0300

    playing with new icons

---
+++ blog-project/src/Icons.jsx
@@ -0,0 +1,24 @@
+import { ReactComponent as NinjaStarIcon } from './images/ninja_star.svg';
+
+
+
+function NinjaIcon() {
+  const iconStyle = {
+    fill: "red", // Change the color of the SVG
+    width: "50px", // Change the size of the SVG
+    height: "50px",
+    border: "20px solid blue", // Change the border thickness and color
+    colorFill: "red",
+  };
+
+  return (
+    <div>
+      <NinjaStarIcon style={iconStyle}/> Using the icon as a component
+    </div>
+  );
+}
+
+export { NinjaIcon };
+
+
+
--- blog-project/src/Navbar.js
+++ blog-project/src/Navbar.js
@@ -1,14 +1,26 @@
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { NinjaIcon } from "./Icons";
+import { postData } from "./useFetch";
+import ColorSwitches from "./testing";
+
+// import { deleteData } from "./useFetch";
+
 const Navbar = () => {
-    return ( 
-        <nav className="navbar">
-            <h1>The Dojo Blog</h1>
-            <div className="links">
-                <Link to="/">Home</Link>
-                <Link to="/create">New Blog</Link>
-            </div>
-        </nav>
-     );
-}
- 
+  return (
+    <nav className="navbar">
+      <div>
+        <h1>
+          The Dojo Blog
+          <NinjaIcon />
+        </h1>
+      </div>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create">New Blog</Link>
+      </div>
+      <handleAddRow />
+    </nav>
+  );
+};
+
 export default Navbar;
