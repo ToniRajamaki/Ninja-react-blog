b942d6f554608ad597f9687db75ee9c11565ce57
TONI <zingoo16@gmail.com>
2023-07-03 13:51:39 +0300

    modified blog details, added button, customized delete button and deleted ID info

--- blog-project/src/BlogDetails.js
+++ blog-project/src/BlogDetails.js
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom/cjs/react-router-dom.min";
 import useFetch from "./useFetch";
 import { deleteData } from "./useFetch";
 import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import ShowDate from "./Date";
 
 const BlogDetails = () => {
   const { id } = useParams();
@@ -13,18 +14,36 @@ const BlogDetails = () => {
     <div className="blog-details">
       <article>
         <h2>{blog.title}</h2>
-        <p style={{ display: "flex", justifyContent: "space-between" }}>
-          <span>Written by {blog.author}</span>
-          <span style={{ fontWeight: "bold", textAlign: "right" }}>
-            id: {id}
-          </span>
-        </p>
+
         <div className="display-linebreak">
           <p style={{ textAlign: "justify" }}>{blog.body}</p>
         </div>
-        <Link to="/">
-          <button onClick={() => deleteData(blog.id)}>DELETE!</button>
-        </Link>
+        <br></br>
+        <p style={{ display: "flex", justifyContent: "space-between" }}>
+          <span>Written by {blog.author}</span>
+          <span style={{ textAlign: "right" }}>
+            {ShowDate(blog.creationDate)}
+          </span>
+        </p>
+        {/* HOME Button */}
+        <div className="container3">
+          <div>
+            <Link to="/">
+              <button> Home</button>
+            </Link>
+          </div>
+          {/* DELETE Button */}
+          <div className="right">
+            <Link to="/">
+              <button
+                className="button2 custom-button"
+                onClick={() => deleteData(blog.id)}
+              >
+                Delete
+              </button>
+            </Link>
+          </div>
+        </div>
       </article>
     </div>
   );
