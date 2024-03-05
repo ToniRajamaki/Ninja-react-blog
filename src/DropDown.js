b3f83cbb1237de7c4533d51d597cf027b41621cf
TONI <zingoo16@gmail.com>
2024-03-05 11:11:47 +0200

    modified navigation bar a lot

---
+++ src/DropDown.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import './Styles/DropDown.css';
+
+function DropDown() {
+    const [isVisible, setIsVisible] = useState(false);
+  
+    // Toggle dropdown visibility
+    const toggleDropdown = () => setIsVisible(!isVisible);
+  
+    // Hide the dropdown
+    const hideDropdown = () => setIsVisible(false);
+  
+    return (
+      <div className="dropdown">
+        <button className="dropbtn" onClick={toggleDropdown}>Ninja Menu</button>
+        <div 
+          className={`dropdown-content ${isVisible ? "show" : ""}`}
+          onMouseLeave={hideDropdown} // Hide dropdown when the cursor leaves the menu
+        >
+          <div className="column">
+            <h4 style={{ color: "var(--main)" }}>Ninjas</h4>
+            {/* Add hideDropdown onClick for each link */}
+            <a href="#" onClick={hideDropdown}>Ninja History</a>
+            <a href="#" onClick={hideDropdown}>Famous Ninjas</a>
+            <a href="#" onClick={hideDropdown}>Ninja Gear</a>
+            <a href="#" onClick={hideDropdown}>Training</a>
+            <a href="#" onClick={hideDropdown}>Missions</a>
+          </div>
+          <div className="column">
+            <h4 style={{ color: "var(--main)" }}>Skills</h4>
+            <a href="#" onClick={hideDropdown}>Stealth</a>
+            <a href="#" onClick={hideDropdown}>Combat</a>
+            <a href="#" onClick={hideDropdown}>Espionage</a>
+            <a href="#" onClick={hideDropdown}>Survival</a>
+            <a href="#" onClick={hideDropdown}>Strategy</a>
+          </div>
+          <div className="column">
+            <h4 style={{ color: "var(--main)" }}>Weapons</h4>
+            <a href="#" onClick={hideDropdown}>Swords</a>
+            <a href="#" onClick={hideDropdown}>Shuriken</a>
+            <a href="#" onClick={hideDropdown}>Nunchaku</a>
+            <a href="#" onClick={hideDropdown}>Blowgun</a>
+            <a href="#" onClick={hideDropdown}>Kunai</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+  
+  export default DropDown;
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,31 +1,45 @@
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
-import { NinjaIcon } from "./Icons";
-import { postData } from "./useFetch";
-import { FaUserNinja } from "react-icons/fa";
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { NinjaIcon } from './Icons'
+import { postData } from './useFetch'
+import { FaUserNinja } from 'react-icons/fa'
+import { FaRegPlusSquare } from "react-icons/fa";
+import { IoIosArrowDropdown } from "react-icons/io";
+import DropDown from './DropDown'
 
 // import { deleteData } from "./useFetch";
 
 const Navbar = () => {
   return (
-    <nav className="navbar" id="home">
+    <nav className='navbar' id='home'>
       <div>
-        <Link to="/">
-          <h1 style={{color:"var(--font-color)", display: "flex", alignItems: "center" }}>
+        <Link to='/'>
+          <h1
+            style={{
+              color: 'var(--font-color)',
+              display: 'flex',
+              alignItems: 'center',
+            }}>
             <FaUserNinja
-              fill="var(--main)"
+              fill='var(--main)'
               size={30}
-              style={{ marginRight: "10px" }}
+              style={{ marginRight: '10px' }}
             />
             Ninja Blog
           </h1>
         </Link>
       </div>
-      <div className="links">
-        <Link to="/">Home</Link>
-        <Link to="/create">New Blog</Link>
+      <div className='links'>
+        <Link to='/'>Home</Link>
+        <Link to='/create' className="centered-link">
+          <FaRegPlusSquare /> New Blog
+        </Link>
+        <span className="centered-link">
+          <DropDown /><IoIosArrowDropdown size={20} />
+        </span>    
+        <span className='profile-pic2'>N</span>
       </div>
     </nav>
-  );
-};
+  )
+}
 
-export default Navbar;
+export default Navbar
